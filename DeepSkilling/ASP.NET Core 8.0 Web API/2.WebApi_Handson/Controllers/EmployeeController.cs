4eefc0009f71c3df78ea29e0a066fdb1c4f4b548
Perala Srujana <srujanaperala276@gmail.com>
2026-07-25 13:06:45 +0530

    Add files via upload

---
+++ DeepSkilling/ASP.NET Core 8.0 Web API/2.WebApi_Handson/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        static List<string> employees = new()
+        {
+            "Dhanush",
+            "Rahul",
+            "Priya",
+            "Anil"
+        };
+
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(string name)
+        {
+            employees.Add(name);
+
+            return Ok(employees);
+        }
+    }
+}
