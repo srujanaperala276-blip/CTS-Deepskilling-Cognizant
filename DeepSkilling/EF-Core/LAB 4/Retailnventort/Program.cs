8fa1686bc1777828c909926827ebd0bbe22b77fd
Perala Srujana <srujanaperala276@gmail.com>
2026-07-25 12:31:03 +0530

    Add files via upload

---
+++ DeepSkilling/EF-Core/LAB 4/Retailnventort/Program.cs
@@ -0,0 +1,39 @@
+﻿using RetailInventory.Data;
+using RetailInventory.Models;
+
+using var context = new AppDbContext();
+
+// Create Categories
+var electronics = new Category
+{
+    Name = "Electronics"
+};
+
+var groceries = new Category
+{
+    Name = "Groceries"
+};
+
+await context.Categories.AddRangeAsync(electronics, groceries);
+
+// Create Products
+var product1 = new Product
+{
+    Name = "Laptop",
+    Price = 75000,
+    Category = electronics
+};
+
+var product2 = new Product
+{
+    Name = "Rice Bag",
+    Price = 1200,
+    Category = groceries
+};
+
+await context.Products.AddRangeAsync(product1, product2);
+
+// Save to Database
+await context.SaveChangesAsync();
+
+Console.WriteLine("Data inserted successfully.");
