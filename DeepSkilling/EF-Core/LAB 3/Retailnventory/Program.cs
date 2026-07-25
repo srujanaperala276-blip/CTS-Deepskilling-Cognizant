fc6daa2f3c3cc8d2105f9a414bcdcc35da7b7f13
Perala Srujana <srujanaperala276@gmail.com>
2026-07-25 12:20:28 +0530

    Add files via upload

---
+++ DeepSkilling/EF-Core/LAB 3/Retailnventory/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====================================");
+            Console.WriteLine(" Retail Inventory Management System ");
+            Console.WriteLine("====================================");
+            Console.WriteLine("Entity Framework Core Setup Completed!");
+            Console.WriteLine("Welcome to EF Core 8.0 Hands-On Lab 1");
+        }
+    }
+}
