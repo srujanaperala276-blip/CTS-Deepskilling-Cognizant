93e480281132605830bc9cf7e2c62cfc32d6cd7a
Perala Srujana <srujanaperala276@gmail.com>
2026-07-26 10:12:48 +0530

    Add files via upload

---
+++ DeepSkilling/React/Lab11_EventExamples/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+import Counter from "./Components/Counter";
+import CurrencyConvertor from "./Components/CurrencyConvertor";
+
+function App() {
+
+  return (
+
+    <div style={{ padding: "20px" }}>
+
+      <Counter />
+
+      <CurrencyConvertor />
+
+    </div>
+
+  );
+
+}
+
+export default App;
