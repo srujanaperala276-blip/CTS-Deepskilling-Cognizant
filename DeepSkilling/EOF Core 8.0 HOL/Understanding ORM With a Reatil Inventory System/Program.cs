d3ce57409489dd95a882516a776ad2038bc72730
Perala Srujana <srujanaperala276@gmail.com>
2026-07-28 16:21:05 +0530

    Add files via upload

---
+++ DeepSkilling/EOF Core 8.0 HOL/Understanding ORM With a Reatil Inventory System/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("================================");
+            Console.WriteLine(" Retail Inventory System");
+            Console.WriteLine("================================");
+            Console.WriteLine("Entity Framework Core Lab 1");
+            Console.WriteLine("ORM Successfully Configured");
+            Console.WriteLine("Project Created Successfully");
+        }
+    }
+}
