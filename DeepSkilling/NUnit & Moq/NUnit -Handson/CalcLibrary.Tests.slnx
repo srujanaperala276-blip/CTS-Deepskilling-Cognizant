7c8476cea7b636fe698c911e661719e2a728d209
Perala Srujana <srujanaperala276@gmail.com>
2026-07-28 16:43:13 +0530

    Add files via upload

---
+++ DeepSkilling/NUnit & Moq/NUnit -Handson/CalcLibrary.Tests.slnx
@@ -0,0 +1,4 @@
+<Solution>
+  <Project Path="CalcLibrary.Tests/CalcLibrary.csproj" />
+  <Project Path="CalcLibrary.Tests1/CalcLibrary.Tests1.csproj" />
+</Solution>
---
+++ DeepSkilling/NUnit & Moq/NUnit -Handson/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using CalcLibrary;
+using NUnit.Framework;
+
+namespace CalcLibrary.Tests1
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            calculator = null;
+        }
+
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 15, 20)]
+        [TestCase(100, 200, 300)]
+        public void Add_Test(int a, int b, int expected)
+        {
+            int actual = calculator.Add(a, b);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
