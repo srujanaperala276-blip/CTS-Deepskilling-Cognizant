25a4bae7b2ad9245dbd1e5b3a185d17bc7037b83
Perala Srujana <srujanaperala276@gmail.com>
2026-07-28 16:24:04 +0530

    Add files via upload

---
+++ DeepSkilling/EOF Core 8.0 HOL/Using EF Core CLI to Create and Apply Migrations/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailMigration.Models;
+
+namespace RetailMigration.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=(localdb)\\MSSQLLocalDB;Database=RetailMigrationDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasPrecision(18, 2);
+        }
+    }
+}
---
+++ DeepSkilling/EOF Core 8.0 HOL/Using EF Core CLI to Create and Apply Migrations/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RetailMigration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retail Migration Project");
+            Console.WriteLine("Entity Framework Core Migration Demo");
+        }
+    }
+}
